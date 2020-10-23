93c1de2a521c46a0856afb1bcf8f450e92d5abe2
Ilgin Dogan <69061934+ilgindgn@users.noreply.github.com>
2020-10-23 00:48:41 -0700

    Update greedy_policy.py

--- policies/greedy_policy.py
+++ policies/greedy_policy.py
@@ -10,9 +10,22 @@ class CentralGreedyPolicy:
         action = self.bipartite_matching()
         return action
 
+    def l2_distance(a_x, a_y, b_x, b_y):
+        a = np.array((a_x, a_y))
+        b = np.array((b_x, b_y))
+        return numpy.linalg.norm(a-b)
+        
     def estimate_rewards(self, drivers, orders):
         # TODO: calculate all possible assignment rewards for each driver
         # self.matching_graph[driver i, order k] = reward for i to take k at this time step
+        for driver in drivers:
+            for order in orders: 
+                dist = l2_distance(driver.x, driver.y, order.x, order.y)
+                if dist <= self.driver_search_radius and dist <= self.order_search_radius: 
+                    _ = driver.try_new_order()
+        
+        
+        
         return self.matching_graph
 
     def bipartite_matching(self):
